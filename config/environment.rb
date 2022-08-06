d9cbb54d7478b57f11c0d5359d1c0fe7f2a9ae6a
AyoubMS <chahayb563@gmail.com>
2022-08-06 21:04:13 +0100

    hide api key

--- config/environment.rb
+++ config/environment.rb
@@ -5,8 +5,8 @@ require_relative "application"
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  :user_name => "apikey", # This is the string literal 'apikey', NOT the ID of your API key
-  :password => "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", # This is the secret sendgrid API key which was issued during API key creation
+  :user_name => Rails.application.credentials.dig(:sendgrid, :username), # This is the string literal 'apikey', NOT the ID of your API key
+  :password => Rails.application.credentials.dig(:sendgrid, :username), # This is the secret sendgrid API key which was issued during API key creation
   :domain => "localhost:3000",
   :address => "smtp.sendgrid.net",
   :port => 465,
