8aed96c223e4ef569c1fc3e70d5f75b9ed61ff91
AyoubMS <chahayb563@gmail.com>
2022-08-08 18:58:35 +0100

    email setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: "app-project-meal.herokuapp.com/" }
+  config.action_mailer.default_url_options = { host: "app-project-meal1.herokuapp.com/" }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
