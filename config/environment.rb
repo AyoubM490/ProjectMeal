3e87b65a4619fc37392e85c040563c255e7252c4
AyoubMS <chahayb563@gmail.com>
2022-08-08 17:11:18 +0100

    remove action_mailer

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,3 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-config.action_mailer.default_url_options = { :host => "app-project-meal.herokuapp.com/", :protocol => "https" }
-config.action_mailer.perform_deliveries = true
-config.action_mailer.delivery_method = :smtp
