9fa90118974ae78d1cdb27a1b6090b778451b3ee
AyoubMS <chahayb563@gmail.com>
2022-08-08 19:09:20 +0100

    email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,16 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: "app-project-meal1.herokuapp.com/" }
+  config.action_mailer.default_url_options = { host: "app-project-meal1.herokuapp.com" }
+  config.action_mailer.smtp_settings = {
+    :user_name => "apikey",
+    :password => Rails.application.credentials.dig(:sendgrid, :password), # This is the secret sendgrid API key which was issued during API key creation
+    :domain => "app-project-meal1.herokuapp.com",
+    :address => "smtp.sendgrid.net",
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true,
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
