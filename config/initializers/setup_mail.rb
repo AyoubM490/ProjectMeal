15f77ec5c575f61c7850951ca3c822cc06aeba2a
AyoubMS <chahayb563@gmail.com>
2022-08-08 18:45:56 +0100

    setup mail

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -3,8 +3,8 @@ ActionMailer::Base.smtp_settings = {
   :address => "smtp.sendgrid.net",
   :port => "587",
   :authentication => :plain,
-  :user_name => "apikey",
-  :password => Rails.application.credentials.dig(:sendgrid, :password),
+  :user_name => Rails.application.credentials.dig(:sendgrid, :heroku_username),
+  :password => Rails.application.credentials.dig(:sendgrid, :heroku_password),
   :domain => "heroku.com",
   :enable_starttls_auto => true,
 }
