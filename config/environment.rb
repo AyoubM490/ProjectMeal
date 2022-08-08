5ce59437e6846f42c295174bb91a8846b7c2f87a
AyoubMS <chahayb563@gmail.com>
2022-08-08 20:29:12 +0100

    email setup

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   :user_name => "apikey", # This is the string literal 'apikey', NOT the ID of your API key
   :password => Rails.application.credentials.dig(:sendgrid, :password), # This is the secret sendgrid API key which was issued during API key creation
-  :domain => "heroku.com",
+  :domain => "app-project-meal1.herokuapp.com",
   :address => "smtp.sendgrid.net",
   :port => 587,
   :authentication => :plain,
