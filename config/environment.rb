6541ad08cfe801eca210c30d205796d9ff53f65d
AyoubMS <chahayb563@gmail.com>
2022-08-08 16:50:14 +0100

    email config

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,6 @@ require_relative "application"
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.smtp_settings = {
-  :user_name => "apikey", # This is the string literal 'apikey', NOT the ID of your API key
-  :password => Rails.application.credentials.dig(:sendgrid, :password), # This is the secret sendgrid API key which was issued during API key creation
-  :domain => "https://app-project-meal.herokuapp.com/",
-  :address => "smtp.sendgrid.net",
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true,
-}
+config.action_mailer.default_url_options = { :host => "app-project-meal.herokuapp.com/", :protocol => "https" }
+config.action_mailer.perform_deliveries = true
+config.action_mailer.delivery_method = :smtp
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,15 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { host: "https://app-project-meal.herokuapp.com/" }
+  ActionMailer::Base.smtp_settings = {
+    :user_name => "apikey", # This is the string literal 'apikey', NOT the ID of your API key
+    :password => Rails.application.credentials.dig(:sendgrid, :password), # This is the secret sendgrid API key which was issued during API key creation
+    :domain => "heroku.com",
+    :address => "smtp.sendgrid.net",
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true,
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
