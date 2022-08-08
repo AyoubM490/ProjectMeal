82cb19c88c6e6f9d76c07d0046fc6b8722c1b5d5
AyoubMS <chahayb563@gmail.com>
2022-08-08 21:26:18 +0100

    email setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,11 +63,19 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "ProjectMeal_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.perform_deliveries = true
-
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: "app-project-meal1.herokuapp.com" }
-
+  host = "app-project-meal1.herokuapp.com"
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :address => "smtp.sendgrid.net",
+    :port => "587",
+    :authentication => :plain,
+    :user_name => "apikey",
+    :password => ENV["SENDGRID_API_KEY"],
+    :domain => "heroku.com",
+    :enable_starttls_auto => true,
+  }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
